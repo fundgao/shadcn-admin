81042527a05fd5be2ae42f8df4cbab7e1db44dfe
Fund <fundgao@gmail.com>
2025-03-01 06:20:23 +0800

    feat: shadcn admin启动

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // TODO if not login, redirect to login page1
+  // TODO if not login, redirect to login page
   return (
     <html lang="zh-CN">
       <body
