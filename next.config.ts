95b5350e26785334b91f548846875e0f68a368e8
Fund <fundgao@gmail.com>
2025-03-18 12:26:41 +0800

    feat: shadcn admin启动

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // DEMO https://github.com/vercel/next-view-transition-example/blob/main/app/card/page.tsx
+    viewTransition: true,
+    useCache: true,
+    turbo: {
+      // Example: adding an alias and custom file extension
+      resolveAlias: {
+        underscore: "lodash",
+      },
+      resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+  },
 };
 
 export default nextConfig;
