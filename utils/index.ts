24a3f6064f03a57535af0fa2225aac4a6c9d3ba3
fundgao <fundgao@gmail.com>
2025-10-12 08:44:22 +0800

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const getItem = (key: string) => {
   if (typeof window !== "undefined") {
     return localStorage.getItem(key);
